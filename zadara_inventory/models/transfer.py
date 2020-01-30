b458a7d427a6c895f00a4f372edb1cd4f260bb1b
Ryan-Zadara <ryan@zadarastorage.com>
2020-01-30 00:48:42 +0000

    zadara_inventory

--- zadara_inventory/models/transfer.py
+++ zadara_inventory/models/transfer.py
@@ -2,6 +2,7 @@
 
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError, UserError
+from datetime import datetime
 
 class transfer(models.Model):
     _name = 'zadara_inventory.transfer'
@@ -24,6 +25,10 @@ class transfer(models.Model):
     serial_number = fields.Char()
     
     quantity = fields.Integer()
+    
+    responsible_party = fields.Selection([('Irvine','Irvine'), ('Yoknaem','Yoknaem')])
+    
+    transfer_date = fields.Date(default=datetime.now())
     
     transfer_tag = fields.Char(readonly=True)
 
@@ -53,6 +58,10 @@ class transfer(models.Model):
                 raise UserError("no destination location")
             if not val.get('product_id'):
                 raise UserError("no product")
+            if val.get('reponsible_party') == '':
+                raise UserError("no responsible party")
+            #if not val.get('transfer_date'):
+            #    val['transfer_date'] = datetime.now()
             track = self.env['zadara_inventory.product'].search([['id','=',val.get("product_id")],['product_trackSerialNumber','=',True]])
             
             if track:            
@@ -63,12 +72,7 @@ class transfer(models.Model):
                 if val.get('serial_number') == 'N/A':
                     raise UserError('bad sns')
                 if self.env['zadara_inventory.master_inventory'].search([['product_id', '=', val.get('product_id')], ['serial_number', '=', val.get('serial_number')],['location_id','=',val.get('source_location_id')],['quantity','=',val.get('quantity')]]):
-                    del val["transfer_name"]
-                    #del vals["product_name"]
                     val['location_id'] = val['destination_location_id']
-                    del val['destination_location_id']
-                    del val['source_location_id']
-                    del val['transfer_type']
                     val['transfer_tag'] = 'write'
                     #do stuff here
                 else:
@@ -82,32 +86,28 @@ class transfer(models.Model):
                 if not self.env['zadara_inventory.master_inventory'].search([['product_id', '=', val.get('product_id')],['location_id','=',val.get('destination_location_id')]]):
                 
                     val['location_id'] = val['destination_location_id']
-                    del val['destination_location_id']
-                    del val['source_location_id']
-                    del val['transfer_type']
-                    del val["transfer_name"]
                     val['transfer_tag'] = 'create'
                 else:
                     other_quantity = self.env['zadara_inventory.master_inventory'].search([['product_id', '=', val.get('product_id')],['location_id','=',val.get('source_location_id')]]).get('quantity')
                     val['quantity'] = other_quantity - quantity 
                     val['location_id'] = val['destination_location_id']
-                    del val['destination_location_id']
-                    del val['source_location_id']
-                    del val['transfer_type']
-                    del val["transfer_name"]
                     val['transfer_tag'] = 'write'
             
         res = super(transfer, self).create(vals_list)
         for vals in vals_list:
-            if vals.get('transfer_tag') == 'write':
-                #del vals["transfer_name"]
-                #del vals["product_name"]
+            del vals["transfer_name"]
+            del vals["product_name"]
                 #vals['location_id'] = vals['destination_location_id']
-                #del vals['destination_location_id']
-                #del vals['source_location_id']
-                #del vals['transfer_type']
+            del vals['destination_location_id']
+            del vals['source_location_id']
+            del vals['transfer_date']
+            del vals['responsible_party']
+            hi = self.env['zadara_inventory.product_history'].create(vals)
+            if vals.get('transfer_tag') == 'write':
+                del vals['transfer_tag']
                 self.write_to_mi(vals)
             else:
+                del vals['transfer_tag']
                 self.create_to_mi(vals)
         return res
     
@@ -128,13 +128,3 @@ class transfer(models.Model):
     def create_to_mi(self, vals_list):
         new_addition = self.env['zadara_inventory.master_inventory'].create(vals_list)
     
-    #def write(self,vals_list):
-    #    if self.serial_number == '1':
-    #        self.serial_number = 'none'
-    #    res = super(update_quantity, self).write(vals_list)
-    #    for vals in vals_list:
-    #        del vals["transfer_name"]
-    #        del vals["product_name"]
-    #        self.create_to_mi(vals)
-    #    return res
-   
