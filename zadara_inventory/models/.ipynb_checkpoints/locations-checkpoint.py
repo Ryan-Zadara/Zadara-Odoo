eaf4df8d3a1f031a36d5bec110675d1e679d5f0c
Ryan-Zadara <ryan@zadarastorage.com>
2020-03-05 17:32:45 +0000

    zadara_inventory

--- zadara_inventory/models/.ipynb_checkpoints/locations-checkpoint.py
+++ zadara_inventory/models/.ipynb_checkpoints/locations-checkpoint.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+from odoo.exceptions import ValidationError, UserError
 
 class locations(models.Model):
     _name = 'zadara_inventory.locations'
@@ -11,3 +11,11 @@ class locations(models.Model):
     address = fields.Char()
     location_type = fields.Selection([('Warehouse','Warehouse'), ('Customer','Customer'),('Vendor','Vendor'),('Lab','Lab')])
     #location_id = fields.Char(readonly="true")
+    @api.model_create_multi
+    def create(self,vals_list):
+        for y in vals_list:
+            for x in self.env['zadara_inventory.locations'].search([]):
+                if x.name == y.get('name'):
+                    raise UserError("Location Already on file")
+        res = super(locations, self).create(vals_list)
+        return res
